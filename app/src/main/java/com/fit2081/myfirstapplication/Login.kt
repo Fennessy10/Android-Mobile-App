991d80aaa4c90ac3312a458e17db040f028c3013
Fennessy10 <patrickfennessy11@gmail.com>
2025-03-16 22:45:08 +1100

    files named correctly

--- app/src/main/java/com/fit2081/myfirstapplication/Login.kt
+++ app/src/main/java/com/fit2081/myfirstapplication/Login.kt
@@ -5,13 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.fit2081.myfirstapplication.ui.theme.MyFirstApplicationTheme
+
+private const val usernameStatic: String = "testUser"
+private const val passwordStatic: String = "testPassword"
 
 class Login : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +19,9 @@ class Login : ComponentActivity() {
         setContent {
             MyFirstApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+                    LoginScreen(usernameStatic, passwordStatic).Screen()
                 }
             }
         }
     }
 }
-
